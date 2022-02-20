d73025391e2fdec23560f2130580aae0702e4a30
Futar <Futarimiti@163.com>
2022-02-20 10:09:14 +0000

    Now write output to csv

--- analyse/ParticipatesAndPerformance.java
+++ analyse/ParticipatesAndPerformance.java
@@ -2,19 +2,27 @@ package analyse;
 
 import static java.util.List.of;
 
+import org.json.JSONObject;
+
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.OptionalDouble;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class ParticipatesAndPerformance
 {
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args) throws IOException
 	{
 		File list = new File("analyse/data/ID-medals.csv");
 		Scanner s = new Scanner(list);
@@ -69,17 +77,88 @@ public class ParticipatesAndPerformance
 			}
 		}
 		
-		System.out.println(freqAndPerformances);
+		freqAndPerformances.remove(0);
+		
+		Map<Integer, List<BigDecimal>> freqAndPerfDens = new TreeMap<>(Integer::compareTo);
+		for (var e : freqAndPerformances.entrySet())
+		{
+			List<BigDecimal> dens = new LinkedList<>();
+			e.getValue().forEach(
+					(v) -> dens.add(BigDecimal.valueOf(v).divide(BigDecimal.valueOf(e.getKey()) , 4 , RoundingMode.UP))
+			);
+			freqAndPerfDens.put(e.getKey() , dens);
+		}
+		
+		System.out.println(freqAndPerfDens);
+		
+		// write out as json
+		JSONObject out = new JSONObject(freqAndPerfDens);
+		Writer f = new FileWriter("analyse/res/FreqAndPerfDens.json");
+		f.write(out.toString());
+		f.close();
 		
 		// get average for all sets
-		Map<Integer , Double> freqAndAvgPerformance = new TreeMap<>(Integer::compareTo);
+		Map<Integer, BigDecimal> freqAndAvgPerfDens = new TreeMap<>(Integer::compareTo);
 		
-		for (Entry<Integer , List<Integer>> e : freqAndPerformances.entrySet())
+		for (Entry<Integer, List<BigDecimal>> e : freqAndPerfDens.entrySet())
+		{
+			BigDecimal[] totalWithCount
+					= e.getValue().stream()
+					.filter(Objects::nonNull)
+					.map(bd -> new BigDecimal[] {bd , BigDecimal.ONE})
+					.reduce((a , b) -> new BigDecimal[] {a[0].add(b[0]) , a[1].add(BigDecimal.ONE)})
+					.get();
+			BigDecimal mean = totalWithCount[0].divide(totalWithCount[1] , RoundingMode.HALF_UP);
+			
+			freqAndAvgPerfDens.put(e.getKey() , mean);
+		}
+		
+		// System.out.println(freqAndAvgPerfDens);
+		// write out as json
+		JSONObject out2 = new JSONObject();
+		try
+		{
+			Field changeMap = out2.getClass().getDeclaredField("map");
+			changeMap.setAccessible(true);
+			changeMap.set(out2 , new TreeMap<>(Integer::compareTo));
+			changeMap.setAccessible(false);
+		} catch (IllegalAccessException | NoSuchFieldException e)
+		{
+			e.printStackTrace();
+		}
+		out2 = new JSONObject(freqAndAvgPerfDens);
+		Writer f1 = new FileWriter("analyse/res/FreqAndAvgPerfDens.json");
+		f1.write(out2.toString());
+		f1.close();
+	}
+}
+		/*
+		// get average for all sets
+		Map<Integer, Double> freqAndAvgPerformance = new TreeMap<>(Integer::compareTo);
+		
+		for (Entry<Integer, List<Integer>> e : freqAndPerformances.entrySet())
 		{
 			OptionalDouble avg = e.getValue().stream().mapToDouble((a) -> a).average();
 			freqAndAvgPerformance.put(e.getKey() , avg.getAsDouble());
 		}
 		
-		System.out.println(freqAndAvgPerformance);
+		freqAndAvgPerformance.remove(0);
+		
+		// write to csv
+		String eol = System.getProperty("line.separator");
+		
+		try (Writer writer = new FileWriter("analyse/res/FrequencyOfParticipationAndPerformance.csv"))
+		{
+			for (Entry<Integer, Double> entry : freqAndAvgPerformance.entrySet())
+			{
+				writer.append(entry.getKey().toString())
+						.append(',')
+						.append(entry.getValue().toString())
+						.append(eol);
+			}
+		} catch (IOException ex)
+		{
+			ex.printStackTrace();
+		}
 	}
-}
+}*/
--- analyse/TotalMedalsPerCountry.java
+++ analyse/TotalMedalsPerCountry.java
@@ -4,7 +4,11 @@ import static java.math.BigDecimal.valueOf;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -55,14 +59,32 @@ public class TotalMedalsPerCountry
 		
 		s.close();
 		
-		Map<String, BigDecimal> percentPerCountry = new TreeMap<>(String::compareTo);
+		Map<String, BigDecimal> percentPerCountryEx0 = new TreeMap<>(String::compareTo);
 		
-		for (Entry<String , Integer> e : countriesAndMedals.entrySet())
+		for (Entry<String, Integer> e : countriesAndMedals.entrySet())
 		{
-			BigDecimal perc = valueOf(e.getValue()).divide(valueOf(total) , 4 , RoundingMode.HALF_UP);
-			percentPerCountry.put(e.getKey() , perc);
+			BigDecimal perc = valueOf(e.getValue()).divide(valueOf(total) , 4 , RoundingMode.HALF_UP).multiply(
+					BigDecimal.valueOf(100)).round(new MathContext(2 , RoundingMode.UP));//.round(new MathContext(2 , RoundingMode.HALF_UP));
+			if (!(perc.floatValue() < 0.01)) percentPerCountryEx0.put(e.getKey() , perc);
 		}
 		
-		System.out.println(percentPerCountry);
+		// write to csv
+		String eol = System.getProperty("line.separator");
+		
+		try (Writer writer = new FileWriter("analyse/res/TotalMetalsPerCountryInPercent.csv"))
+		{
+			for (Map.Entry<String, BigDecimal> entry : percentPerCountryEx0.entrySet())
+			{
+				writer.append(entry.getKey())
+						.append(',')
+						.append(entry.getValue().toString())
+						.append(eol);
+			}
+		} catch (IOException ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		
 	}
 }
